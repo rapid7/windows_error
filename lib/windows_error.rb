42a295476e2e08d3b7a64ee9cbb903a0f22a3339
Spencer McIntyre <zeroSteiner@gmail.com>
2024-12-06 13:23:39 -0500

    Add missing require statements

--- lib/windows_error.rb
+++ lib/windows_error.rb
@@ -1,4 +1,8 @@
-require 'windows_error/version'
 require 'windows_error/error_code'
+require 'windows_error/win32'
+require 'windows_error/nt_status'
+require 'windows_error/h_result'
+require 'windows_error/version'
+
 module WindowsError
 end
